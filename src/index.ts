e7a134ad6c7e336399db00f24aad19404e53bb02
mterrera29 <matiasterrera29@gmail.com>
2025-07-01 21:34:00 -0300

    inicio

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import server from './server';
+
+server.listen(4000, () => {
+  console.log('REST API EN EL PUERTO 4000');
+});
--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from 'express';
 
 const server = express();
 
+server.get('/', (req, res) => {
+  res.send('holaaa pibe');
+});
+
 export default server;
