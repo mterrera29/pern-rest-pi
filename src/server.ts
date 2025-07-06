e49fea88896ec9d46f5080e4cae19bce275192f2
mterrera29 <matiasterrera29@gmail.com>
2025-07-06 19:25:17 -0300

    inicio

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,18 @@ import express from 'express';
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send('holaaa pibe');
+  res.json('desde get');
 });
 
+server.post('/', (req, res) => {
+  res.json('desde post');
+});
+
+server.put('/', (req, res) => {
+  res.json('desde put');
+});
+
+server.delete('/', (req, res) => {
+  res.json('desde delete');
+});
 export default server;
