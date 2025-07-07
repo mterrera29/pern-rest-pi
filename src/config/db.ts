b80c8d2237d986a2026b3d20458ef2ca36493d9c
mterrera29 <matiasterrera29@gmail.com>
2025-07-07 20:41:55 -0300

    inicio

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize-typescript';
 import dotenv from 'dotenv';
 dotenv.config();
 
---
+++ src/models/Product.model.ts
@@ -0,0 +1,21 @@
+import { Table, Column, Model, DataType, Default } from 'sequelize-typescript';
+
+@Table({
+  tableName: 'products',
+})
+class Product extends Model {
+  @Column({
+    type: DataType.STRING(100),
+  })
+  name: string;
+
+  @Column({
+    type: DataType.FLOAT(6, 2),
+  })
+  price: number;
+
+  @Column({
+    type: DataType.BOOLEAN,
+  })
+  availability: boolean;
+}
