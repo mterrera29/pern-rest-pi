d795ab1f914fbd3e1dfec8dea9e95fbee02fb3b3
mterrera29 <matiasterrera29@gmail.com>
2025-07-06 22:22:33 -0300

    inicio

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import server from './server';
-
+const port = process.env.PORT || 4000;
 server.listen(4000, () => {
-  console.log('REST API EN EL PUERTO 4000');
+  console.log(`REST API EN EL PUERTO ${port}`);
 });
