145c71460e068556f69c7381e14e6bedb722b256
mterrera29 <matiasterrera29@gmail.com>
2025-07-07 21:01:41 -0300

    inicio

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,6 +2,8 @@ import { Sequelize } from 'sequelize-typescript';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new Sequelize(process.env.RENDER_URL);
+const db = new Sequelize(process.env.RENDER_URL, {
+  models: [__dirname + '/../models/**/*.ts'],
+});
 
 export default db;
