df5882809c8abd5ab4e90f9088ed086834babb93
mterrera29 <matiasterrera29@gmail.com>
2025-07-08 21:41:01 -0300

    inicio

---
+++ src/handlers/product.ts
@@ -0,0 +1,3 @@
+export const createProduct = (req, res) => {
+  res.json('desde post');
+};
--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { createProduct } from './handlers/product';
 
 const router = Router();
 
@@ -6,9 +7,7 @@ router.get('/', (req, res) => {
   res.json('desde get');
 });
 
-router.post('/', (req, res) => {
-  res.json('desde post');
-});
+router.post('/', createProduct);
 
 router.put('/', (req, res) => {
   res.json('desde put');
