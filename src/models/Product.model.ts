0a960a89f96af47c6dd2619ede47339deddedc68
mterrera29 <matiasterrera29@gmail.com>
2025-07-08 22:45:15 -0300

    inicio

--- src/models/Product.model.ts
+++ src/models/Product.model.ts
@@ -7,17 +7,17 @@ class Product extends Model {
   @Column({
     type: DataType.STRING(100),
   })
-  declare name: string;
+  name: string;
 
   @Column({
     type: DataType.FLOAT(6, 2),
   })
-  declare price: number;
+  price: number;
 
   @Default(true)
   @Column({
     type: DataType.BOOLEAN,
   })
-  declare availability: boolean;
+  availability: boolean;
 }
 export default Product;
