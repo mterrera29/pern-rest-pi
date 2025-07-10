55943f6293cb8236c53d770c1285c47ec4b924ef
mterrera29 <matiasterrera29@gmail.com>
2025-07-10 10:27:27 -0300

    inicio

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -37,7 +37,19 @@ export const createProduct = async (req: Request, res: Response) => {
   }
 };
 
-export const updateProduct = async (req, res) => {
-  const product = req.params;
-  res.json({ data: product });
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByPk(id);
+
+    if (!product) {
+      res.status(404).json({ error: 'Producto no encontrado' });
+    }
+    await product.update(req.body);
+    await product.save();
+
+    res.json({ data: product });
+  } catch (error) {
+    console.log(error);
+  }
 };
