35f891dc45ddf6967afb4e6e2b0ce25a4797fb03
mterrera29 <matiasterrera29@gmail.com>
2025-07-11 21:12:46 -0300

    inicio

--- src/__test__/server.test.ts
+++ src/__test__/server.test.ts
@@ -4,6 +4,7 @@ import server from '../server';
 describe('GET /api', () => {
   it('should send back a json response', async () => {
     const res = await request(server).get('/api');
-    console.log(res);
+    expect(res.status).toBe(200);
+    console.log(res.status);
   });
 });
