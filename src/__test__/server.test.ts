4f8ffc2d495d164214e6c8f9b5a18364aa909537
mterrera29 <matiasterrera29@gmail.com>
2025-07-12 23:36:39 -0300

    inicio

--- src/__test__/server.test.ts
+++ src/__test__/server.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import server from '../server';
 
 describe('GET /api', () => {
-  it('should send back a json response', async () => {
+  it('should send back a json res', async () => {
     const res = await request(server).get('/api');
     expect(res.status).toBe(200);
     expect(res.headers['content-type']).toMatch(/json/);
